b89161505066681ba434c6efb3448127e38adffd
liviatanaka <liviatanaka19@gmail.com>
2023-04-04 13:27:53 -0300

    controle da rotacao por wasdqe

--- Cubo.py
+++ Cubo.py
@@ -7,7 +7,7 @@ class Cubo():
 
     def __init__(self):
         '''
-        Inicializa a classe Cubo com os vértices de um cubo unitário centrado na origem.
+        Inicializa a classe Cubo com os vértices e as arestas de um cubo unitário centrado na origem.
         '''
         self.vertices = np.array([[-1, -1, -1, 1],
                                   [ 1, -1, -1, 1],
@@ -18,12 +18,25 @@ class Cubo():
                                   [ 1,  1,  1, 1],
                                   [-1,  1,  1, 1]])
         
+        self.arestas = np.array([[0, 1], 
+                                 [1, 2], 
+                                 [2, 3], 
+                                 [3, 0], 
+                                 [4, 5], 
+                                 [5, 6], 
+                                 [6, 7], 
+                                 [7, 4], 
+                                 [0, 4], 
+                                 [1, 5], 
+                                 [2, 6], 
+                                 [3, 7]])
+        
     def matriz_rotacao_x(self, theta):
         '''
         Retorna uma matriz de rotação em torno do eixo x.
 
         Args:
-            theta (float): ângulo de rotação em graus.
+            theta (float): ângulo de rotação em radianos.
 
         Returns:
             R_x (numpy array): matriz de rotação em torno do eixo x.
@@ -40,7 +53,7 @@ class Cubo():
         Retorna uma matriz de rotação em torno do eixo y.
 
         Args:
-            theta (float): ângulo de rotação em graus.
+            theta (float): ângulo de rotação em radianos.
 
         Returns:
             R_y (numpy array): matriz de rotação em torno do eixo y.
@@ -57,7 +70,7 @@ class Cubo():
         Retorna uma matriz de rotação em torno do eixo z.
 
         Args:
-            theta (float): ângulo de rotação em graus.
+            theta (float): ângulo de rotação em radianos.
 
         Returns:
             R_z (numpy array): matriz de rotação em torno do eixo z.
@@ -83,7 +96,7 @@ class Cubo():
                 [0, 0, 0, 1]])
         return T
 
-    def rotacao_cubo(self, ang):
+    def rotacao_cubo(self, angulos, modo):
         '''
         Aplica rotação sobre o cubo.
 
@@ -95,12 +108,23 @@ class Cubo():
 
         '''
         objeto = self.vertices.T
-        theta = np.radians(ang)
-
         T = self.matriz_translacao()
-        R_x = self.matriz_rotacao_x(theta)
-        R_y = self.matriz_rotacao_y(theta)
-        R_z = self.matriz_rotacao_z(theta)
+
+        if modo == "padrao":
+            theta = np.radians(angulos["todos"])
+
+            R_x = self.matriz_rotacao_x(theta)
+            R_y = self.matriz_rotacao_y(theta)
+            R_z = self.matriz_rotacao_z(theta)
+        
+        elif modo == "controle":
+            theta_x = np.radians(angulos["x"])
+            theta_y = np.radians(angulos["y"])
+            theta_z = np.radians(angulos["z"])
+
+            R_x = self.matriz_rotacao_x(theta_x)
+            R_y = self.matriz_rotacao_y(theta_y)
+            R_z = self.matriz_rotacao_z(theta_z)
 
         R = T @ R_x @ R_y @ R_z 
         objeto_rotacionado = R @ objeto
@@ -115,8 +139,8 @@ class Cubo():
             objeto_rotacionado (numpy array): objeto rotacionado.
 
         Returns:
-            xp (numpy array): coordenada x do ponto projetado.
-            yp (numpy array): coordenada y do ponto projetado.
+            xp (numpy array): lista de coordenada x do ponto projetado.
+            yp (numpy array): lista de coordenada y do ponto projetado.
 
         '''
         P = np.array([[1, 0, 0, 0], 
--- Tela_cubo.py
+++ Tela_cubo.py
@@ -13,20 +13,19 @@ class Tela_cubo():
         Construtor da classe Tela_cubo.
 
         Atributos:
-            pa (int): valor inicial é zero.
             cubo (Cubo): objeto da classe Cubo, que será renderizado na tela.
             ang (int): valor inicial é zero, utilizado para a rotação do cubo.
             ang_x (int): valor inicial é zero, utilizado para a rotação do cubo no eixo x.
             vel (int): valor inicial é um, representa a velocidade de rotação do cubo.
             d (int): valor inicial é 400, representa a distância da câmera ao cubo.
         """
-        self.pa = 0
         self.cubo = Cubo()
-        self.ang = 0
-        self.ang_x = 0
+        self.angulos = {"x": 0, "y": 0, "z": 0, "todos": 0}
         self.vel = 1
         self.d = 400
-
+        self.modo = "padrao"
+        # padrão -> rotaciona normal em todos os eixos
+        # controle -> rotaciona pelas teclas
     def atualiza(self):
         """
         Método que atualiza o ângulo de rotação do cubo e realiza ações do teclado e mouse.
@@ -37,22 +36,39 @@ class Tela_cubo():
 
         """
 
-        self.ang += self.vel
+        self.angulos["todos"] += self.vel
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return None
             if event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_i:
-                    pass
-                if event.key == pygame.K_RIGHT:
-                    self.ang_x += self.vel
+                if event.key == pygame.K_p:
+                    self.modo = "padrao"
+                if event.key == pygame.K_c:
+                    self.modo = "controle"
+                
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4:  # zoom in
                     self.d *= 1.1
                 elif event.button == 5:  # zoom out
                     self.d /= 1.1
-        
+
+
+        if self.modo == "controle":
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                self.angulos["x"] += self.vel
+            if keys[pygame.K_s]:
+                self.angulos["x"] -= self.vel
+            if keys[pygame.K_d]:
+                self.angulos["y"] += self.vel
+            if keys[pygame.K_a]:
+                self.angulos["y"] -= self.vel
+            if keys[pygame.K_q]:
+                self.angulos["z"] += self.vel
+            if keys[pygame.K_e]:
+                self.angulos["z"] -= self.vel
+
         return self
 
     def desenha(self, screen):
@@ -64,18 +80,20 @@ class Tela_cubo():
 
         """
         
-        objeto_rotacionado = self.cubo.rotacao_cubo(self.ang)
+        objeto_rotacionado = self.cubo.rotacao_cubo(self.angulos, self.modo)
         xp, yp = self.cubo.projecao_cubo(self.d, objeto_rotacionado)
 
         # desenho ##########################################################################################################
         
         screen.fill(PRETO)
-        aresta = np.array([[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]])
         i = 0
-        while i < len(aresta):
-            x = aresta[i]
+
+        largura = WIDTH//2
+        altura = HEIGHT//2
+        while i < len(self.cubo.arestas):
+            x = self.cubo.arestas[i]
             # Desenha uma linha entre os dois pontos da aresta e coloca na tela.
-            pygame.draw.line(screen, AZUL, (xp[x[0]] + WIDTH//2, yp[x[0]] + HEIGHT//2), (xp[x[1]] + WIDTH//2, yp[x[1]] + HEIGHT//2), 5)
+            pygame.draw.line(screen, AZUL, (xp[x[0]] + largura, yp[x[0]] + altura), (xp[x[1]] +largura, yp[x[1]] + altura), 5)
             i += 1
 
         i = 0
@@ -83,6 +101,6 @@ class Tela_cubo():
             x = xp[i]
             y = yp[i]
             # Desenha um circulo no ponto e coloca na tela.
-            pygame.draw.circle(screen, VERM, (x +WIDTH//2, y + HEIGHT//2), 5)
+            pygame.draw.circle(screen, VERM, (x + largura, y + altura), 5)
 
             i += 1
